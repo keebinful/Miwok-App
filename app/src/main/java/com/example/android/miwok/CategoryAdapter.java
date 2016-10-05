726c3aeecdeeab333cc7358ae120ce8a40048176
Kevin Liu <kirikliu@gmail.com>
2016-10-04 17:27:09 -0700

    Can now swipe between fragments; removed activities
    TODO: Add tabs

---
+++ app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -0,0 +1,35 @@
+package com.example.android.miwok;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by kirik_000 on 10/4/2016.
+ */
+
+public class CategoryAdapter extends FragmentPagerAdapter {
+    public CategoryAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new NumbersFragment();
+            case 1:
+                return new FamilyFragment();
+            case 2:
+                return new ColorsFragment();
+            case 3:
+                return new PhrasesFragment();
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+}
